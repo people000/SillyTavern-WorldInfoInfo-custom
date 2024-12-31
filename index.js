390194ce5e4c5c5f71db83048615747f1c493182
LenAnderson <anderson.len.ja@gmail.com>
2024-12-31 19:16:04 -0400

    add messages

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import { event_types, eventSource, saveSettingsDebounced } from '../../../../script.js';
+import { chat, chat_metadata, event_types, eventSource, main_api, saveSettingsDebounced } from '../../../../script.js';
+import { metadata_keys } from '../../../authors-note.js';
 import { extension_settings } from '../../../extensions.js';
-import { executeSlashCommandsWithOptions } from '../../../slash-commands.js';
+import { promptManager } from '../../../openai.js';
 import { SlashCommandParser } from '../../../slash-commands/SlashCommandParser.js';
 import { delay } from '../../../utils.js';
+import { world_info_position } from '../../../world-info.js';
 
 const strategy = {
     constant: '🔵',
@@ -19,17 +21,20 @@ const getStrategy = (entry)=>{
     }
 };
 
+let generationType;
+eventSource.on(event_types.GENERATION_STARTED, (genType)=>generationType = genType);
+
 const init = ()=>{
     const trigger = document.createElement('div'); {
         trigger.classList.add('stwii--trigger');
         trigger.classList.add('fa-solid', 'fa-fw', 'fa-book-atlas');
         trigger.title = 'Active WI\n---\nright click for options';
         trigger.addEventListener('click', ()=>{
-            panel.style.display = panel.style.display == '' ? 'flex' : '';
+            panel.classList.toggle('stwii--isActive');
         });
         trigger.addEventListener('contextmenu', (evt)=>{
             evt.preventDefault();
-            configPanel.style.display = configPanel.style.display == '' ? 'flex' : '';
+            configPanel.classList.toggle('stwii--isActive');
         });
         document.body.append(trigger);
     }
@@ -84,10 +89,33 @@ const init = ()=>{
             }
             configPanel.append(orderRow);
         }
+        const mesRow = document.createElement('label'); {
+            mesRow.classList.add('stwii--configRow');
+            mesRow.title = 'Indicate message history (only when ungrouped and shown in order)';
+            const cb = document.createElement('input'); {
+                cb.type = 'checkbox';
+                cb.checked = extension_settings.worldInfoInfo?.mes ?? true;
+                cb.addEventListener('click', ()=>{
+                    if (!extension_settings.worldInfoInfo) {
+                        extension_settings.worldInfoInfo = {};
+                    }
+                    extension_settings.worldInfoInfo.mes = cb.checked;
+                    updatePanel(currentEntryList);
+                    saveSettingsDebounced();
+                });
+                mesRow.append(cb);
+            }
+            const lbl = document.createElement('div'); {
+                lbl.textContent = 'Show messages';
+                mesRow.append(lbl);
+            }
+            configPanel.append(mesRow);
+        }
         document.body.append(configPanel);
     }
 
     let entries = [];
+
     let count = -1;
     const updateBadge = async(newEntries)=>{
         if (count != newEntries.length) {
@@ -116,49 +144,56 @@ const init = ()=>{
         entries = newEntries;
     };
     let currentEntryList = [];
+    let currentChat = [];
     eventSource.on(event_types.WORLD_INFO_ACTIVATED, async(entryList)=>{
         panel.innerHTML = 'Updating...';
         updateBadge(entryList.map(it=>`${it.world}§§§${it.uid}`));
         for (const entry of entryList) {
-            entry.sticky = parseInt(await SlashCommandParser.commands['wi-get-timed-effect'].callback(
+            entry.type = 'wi';
+            entry.sticky = parseInt(/**@type {string}*/(await SlashCommandParser.commands['wi-get-timed-effect'].callback(
                 {
                     effect: 'sticky',
                     format: 'number',
-                    file: entry.world,
+                    file: `${entry.world}`,
+                    _scope: null,
+                    _abortController: null,
                 },
                 entry.uid,
-            ));
+            )));
         }
         currentEntryList = [...entryList];
-        updatePanel(entryList);
+        updatePanel(entryList, true);
     });
-    const updatePanel = (entryList)=>{
+
+
+    const updatePanel = (entryList, newChat = false)=>{
+        const isGrouped = extension_settings.worldInfoInfo?.group ?? true;
+        const isOrdered = extension_settings.worldInfoInfo?.order ?? true;
+        const isMes = extension_settings.worldInfoInfo?.mes ?? true;
         panel.innerHTML = '';
         let grouped;
-        if (extension_settings.worldInfoInfo?.group ?? true) {
+        if (isGrouped) {
             grouped = Object.groupBy(entryList, (it,idx)=>it.world);
         } else {
             grouped = {
-                'WI Entries': entryList,
+                'WI Entries': [...entryList],
             };
         }
-        trigger.style.display = 'block';
+        const depthPos = [world_info_position.ANBottom, world_info_position.ANTop, world_info_position.atDepth];
         for (const [world, entries] of Object.entries(grouped)) {
+            for (const e of entries) {
+                e.depth = e.position == world_info_position.atDepth ? e.depth : (chat_metadata[metadata_keys.depth] + (e.position == world_info_position.ANTop ? 0.1 : 0));
+            }
             const w = document.createElement('div'); {
-                w.classList.add('stwiii--world');
+                w.classList.add('stwii--world');
                 w.textContent = world;
-                const style = {
-                    fontWeight: 'bold',
-                };
-                for (const [k, v] of Object.entries(style)) {
-                    w.style.setProperty(k.replace(/[A-Z]/g, (c)=>`-${c.toLowerCase()}`), v);
-                }
                 panel.append(w);
                 entries.sort((a,b)=>{
-                    if (extension_settings.worldInfoInfo?.order ?? true) {
+                    if (isOrdered) {
                         // order by strategy / depth / order
-                        if (a.position > b.position) return 1;
-                        if (a.position < b.position) return -1;
+                        if (!depthPos.includes(a.position) && !depthPos.includes(b.position)) return a.position - b.position;
+                        if (depthPos.includes(a.position) && !depthPos.includes(b.position)) return 1;
+                        if (!depthPos.includes(a.position) && depthPos.includes(b.position)) return -1;
                         if ((a.depth ?? Number.MAX_SAFE_INTEGER) < (b.depth ?? Number.MAX_SAFE_INTEGER)) return 1;
                         if ((a.depth ?? Number.MAX_SAFE_INTEGER) > (b.depth ?? Number.MAX_SAFE_INTEGER)) return -1;
                         if ((a.order ?? Number.MAX_SAFE_INTEGER) > (b.order ?? Number.MAX_SAFE_INTEGER)) return 1;
@@ -172,20 +207,155 @@ const init = ()=>{
                         ;
                     }
                 });
+                if (!isGrouped && isOrdered && isMes) {
+                    const an = chat_metadata[metadata_keys.prompt];
+                    const ad = chat_metadata[metadata_keys.depth];
+                    if (an?.length) {
+                        const idx = entries.findIndex(e=>depthPos.includes(e.position) && e.depth <= ad);
+                        entries.splice(idx, 0, {
+                            type: 'note',
+                            position: world_info_position.ANBottom,
+                            depth: ad,
+                            text: an,
+                        });
+                    }
+                    if (newChat) {
+                        currentChat = [...chat];
+                        if (generationType == 'swipe') currentChat.pop();
+                    }
+                    const segmenter = new Intl.Segmenter('en', { granularity:'sentence' });
+                    let currentDepth = currentChat.length - 1;
+                    let isDumped = false;
+                    for (let i = entries.length - 1; i >= -1; i--) {
+                        if (i < 0 && currentDepth < 0) continue;
+                        if (isDumped) continue;
+                        if ((i < 0 && currentDepth >= 0) || !depthPos.includes(entries[i].position)) {
+                            // anything not @D is considered as "before chat"
+                            isDumped = true;
+                            const depth = -1;
+                            const mesList = currentChat.slice(depth + 1, currentDepth + 1);
+                            const text = mesList
+                                .map(it=>it.mes)
+                                .map(it=>it
+                                    .replace(/```.+```/gs, '')
+                                    .replace(/<[^>]+?>/g, '')
+                                    .trim()
+                                    ,
+                                )
+                                .filter(it=>it.length)
+                                .join('\n')
+                            ;
+                            const sentences = [...segmenter.segment(text)].map(it=>it.segment.trim());
+                            entries.splice(i + 1, 0, {
+                                type: 'mes',
+                                count: mesList.length,
+                                from: depth + 1,
+                                to: currentDepth,
+                                first: sentences.at(0),
+                                last: sentences.length > 1 ? sentences.at(-1) : null,
+                            });
+                            currentDepth = -1;
+                            continue;
+                        }
+                        let depth = Math.max(-1, currentChat.length - entries[i].depth - 1);
+                        if (depth >= currentDepth) continue;
+                        depth = Math.ceil(depth);
+                        if (depth == currentDepth) continue;
+                        const mesList = currentChat.slice(depth + 1, currentDepth + 1);
+                        const text = mesList
+                            .map(it=>it.mes)
+                            .map(it=>it
+                                .replace(/```.+```/gs, '')
+                                .replace(/<[^>]+?>/g, '')
+                                .trim()
+                                ,
+                            )
+                            .filter(it=>it.length)
+                            .join('\n')
+                        ;
+                        const sentences = [...segmenter.segment(text)].map(it=>it.segment.trim());
+                        entries.splice(i + 1, 0, {
+                            type: 'mes',
+                            count: mesList.length,
+                            from: depth + 1,
+                            to: currentDepth,
+                            first: sentences.at(0),
+                            last: sentences.length > 1 ? sentences.at(-1) : null,
+                        });
+                        currentDepth = depth;
+                    }
+                }
                 for (const entry of entries) {
-                    // const sticky = parseInt((await executeSlashCommandsWithOptions(`/wi-get-timed-effect effect=sticky format=number file="${world.replace(/"/g, '\\"')}" ${entry.uid}`)).pipe);
                     const e = document.createElement('div'); {
-                        e.classList.add('stwiii--entry');
-                        e.textContent = [
-                            strategy[getStrategy(entry)],
-                            entry.comment?.length ? entry.comment : entry.key.join(', '),
-                            entry.sticky ? `📌 ${entry.sticky}` : null,
-                        ].filter(it=>it).join(' ');
-                        const style = {
-                            paddingLeft: '1em',
-                        };
-                        for (const [k, v] of Object.entries(style)) {
-                            e.style.setProperty(k.replace(/[A-Z]/g, (c)=>`-${c.toLowerCase()}`), v);
+                        e.classList.add('stwii--entry');
+                        const wipChar = [world_info_position.before, world_info_position.after];
+                        const wipEx = [world_info_position.EMTop, world_info_position.EMBottom];
+                        // not needed after all?
+                        if (false && [...wipChar, ...wipEx].includes(entry.position)) {
+                            if (main_api == 'openai') {
+                                const pm = promptManager.getPromptCollection().collection;
+                                if (wipChar.includes(entry.position) && !pm.find(it=>it.identifier == 'charDescription')) {
+                                    e.classList.add('stwii--isBroken');
+                                    e.title = '⚠️ Not sent because position anchor is missing (Char Description)!\n';
+                                } else if (wipEx.includes(entry.position) && !pm.find(it=>it.identifier == 'dialogueExamples')) {
+                                    e.classList.add('stwii--isBroken');
+                                    e.title = '⚠️ Not sent because position anchor is missing (Example Messages)!\n';
+                                }
+                            }
+                        } else {
+                            e.title = '';
+                        }
+                        if (entry.type == 'mes') e.classList.add('stwii--messages');
+                        if (entry.type == 'note') e.classList.add('stwii--note');
+                        const strat = document.createElement('div'); {
+                            strat.classList.add('stwii--strategy');
+                            if (entry.type == 'wi') {
+                                strat.textContent = strategy[getStrategy(entry)];
+                            } else if (entry.type == 'mes') {
+                                strat.classList.add('fa-solid', 'fa-fw', 'fa-comments');
+                                strat.setAttribute('data-stwii--count', entry.count.toString());
+                            } else if (entry.type == 'note') {
+                                strat.classList.add('fa-solid', 'fa-fw', 'fa-note-sticky');
+                            }
+                            e.append(strat);
+                        }
+                        const title = document.createElement('div'); {
+                            title.classList.add('stwii--title');
+                            if (entry.type == 'wi') {
+                                title.textContent = entry.comment?.length ? entry.comment : entry.key.join(', ');
+                                e.title += `[${entry.world}] ${entry.comment?.length ? entry.comment : entry.key.join(', ')}\n---\n${entry.content}`;
+                            } else if (entry.type == 'mes') {
+                                const first = document.createElement('div'); {
+                                    first.classList.add('stwii--first');
+                                    first.textContent = entry.first;
+                                    title.append(first);
+                                }
+                                if (entry.last) {
+                                    e.title = `Messages #${entry.from}-${entry.to}\n---\n${entry.first}\n...\n${entry.last}`;
+                                    const sep = document.createElement('div'); {
+                                        sep.classList.add('stwii--sep');
+                                        sep.textContent = '...';
+                                        title.append(sep);
+                                    }
+                                    const last = document.createElement('div'); {
+                                        last.classList.add('stwii--last');
+                                        last.textContent = entry.last;
+                                        title.append(last);
+                                    }
+                                } else {
+                                    e.title = `Message #${entry.from}\n---\n${entry.first}`;
+                                }
+                            } else if (entry.type == 'note') {
+                                title.textContent = 'Author\'s Note';
+                                e.title = `Author's Note\n---\n${entry.text}`;
+                            }
+                            e.append(title);
+                        }
+                        const sticky = document.createElement('div'); {
+                            sticky.classList.add('stwii--sticky');
+                            sticky.textContent = entry.sticky ? `📌 ${entry.sticky}` : '';
+                            sticky.title = `Sticky for ${entry.sticky} more rounds`;
+                            e.append(sticky);
                         }
                         panel.append(e);
                     }
@@ -198,8 +368,6 @@ const init = ()=>{
     const original_debug = console.debug;
     console.debug = function(...args) {
         if (args[0] == '[WI] Found 0 world lore entries. Sorted by strategy') {
-            trigger.style.display = 'none';
-            panel.style.display = 'none';
             panel.innerHTML = 'No active entries';
         }
         return original_debug.bind(this)(...args);
